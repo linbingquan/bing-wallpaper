5b1fd75b7c667078c01d2364dddb3c568ea3b020
linbingquan <695601626@qq.com>
2025-01-29 19:03:12 +0800

    feat: mkt=zh-CN

--- wallpapers.ts
+++ wallpapers.ts
@@ -10,43 +10,53 @@ interface Wallpaper {
   url?: string;
 }
 
-/** bing 网址 */
-const BING_URL = "https://www.bing.com";
+async function fetchWallpapers(BING_URL: string, mkt = "zh-CN") {
+  mkt = BING_URL.includes("cn") ? `&mkt=${mkt}` : "";
+  /** bing 壁纸地址 API */
+  const BING_API =
+    `${BING_URL}/HPImageArchive.aspx?format=js&idx=0&n=8&uhd=1&uhdwidth=3840&uhdheight=2160` +
+    mkt;
 
-/** bing 壁纸地址 API */
-const BING_API =
-  `${BING_URL}/HPImageArchive.aspx?format=js&idx=0&n=8&uhd=1&uhdwidth=3840&uhdheight=2160`;
+  const images = await fetch(BING_API)
+    .then((res) => res.json())
+    .then((json: { images: Wallpaper[] }) => json.images);
 
-const images = await fetch(BING_API)
-  .then((res) => res.json())
-  .then((json: { images: Wallpaper[] }) => json.images);
+  images.forEach((image) => {
+    const {
+      url = "", // 图片地址
+      enddate = "", // 结束图片时间
+      copyright = "", // 图片版权
+    } = image;
 
-images.forEach((image) => {
-  const {
-    url = "", // 图片地址
-    enddate = "", // 结束图片时间
-    copyright = "", // 图片版权
-  } = image;
+    // 查找最新的壁纸
+    const wallpaper = wallpapers.find((item) =>
+      new URL(item.url).searchParams.get("id") ===
+        new URL(`${BING_URL}${url}`).searchParams.get("id")
+    );
+    // 没找到今天的壁纸
+    if (wallpaper === undefined) {
+      // 追加今天的壁纸
+      const originUrl = url.slice(0, url.indexOf("&"));
 
-  // 查找最新的壁纸
-  const wallpaper = wallpapers.find((item) => item.enddate === enddate);
-  // 没找到今天的壁纸
-  if (wallpaper === undefined) {
-    // 追加今天的壁纸
-    const originUrl = url.slice(0, url.indexOf("&"));
+      const wallpaper = {
+        enddate: enddate,
+        copyright,
+        url: `${BING_URL}${originUrl}`,
+      };
+      console.log(wallpaper);
+      wallpapers.push(wallpaper);
+    }
+  });
 
-    const wallpaper = {
-      enddate: enddate,
-      copyright,
-      url: `${BING_URL}${originUrl}`,
-    };
-    wallpapers.push(wallpaper);
-  }
-});
+  // 格式化 json 并写入文件
+  const array = wallpapers.sort((a, b) =>
+    Number(a.enddate) - Number(b.enddate)
+  );
+  const string = JSON.stringify(array, null, 2);
+  const path = "./fresh/routes/wallpapers.json";
+  Deno.writeTextFileSync(path, string);
+  console.log(`${BING_URL} -> ${path} 更新成功`);
+}
 
-// 格式化 json 并写入文件
-const array = wallpapers.sort((a, b) => Number(a.enddate) - Number(b.enddate));
-const string = JSON.stringify(array, null, 2);
-const path = "./fresh/routes/wallpapers.json";
-Deno.writeTextFileSync(path, string);
-console.log(`${path} 更新成功`);
+await fetchWallpapers("https://cn.bing.com");
+await fetchWallpapers("https://www.bing.com");
