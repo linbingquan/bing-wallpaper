2d88badb7c8b9f1388d85cc02d20a9cd70c2b6d2
linbingquan <695601626@qq.com>
2025-02-26 15:41:45 +0800

    0.2.2

--- fresh/routes/index.tsx
+++ fresh/routes/index.tsx
@@ -1,18 +1,33 @@
 import Wallpaper from "../islands/Wallpaper.tsx";
 import wallpapers from "./wallpapers.json" with { type: "json" };
 
-wallpapers.sort((a, b) => Number(b.enddate) - Number(a.enddate));
+const cn = wallpapers.filter((item) => item.url.includes("cn.bing.com"));
+const www = wallpapers.filter((item) => item.url.includes("www.bing.com"));
+
+cn.sort((a, b) => Number(b.enddate) - Number(a.enddate));
+www.sort((a, b) => Number(b.enddate) - Number(a.enddate));
 
 export default function Home() {
   return (
-    <div class="bing-wallpaper">
-      {wallpapers.map((item) => (
-        <Wallpaper
-          url={item.url}
-          enddate={item.enddate}
-          copyright={item.copyright}
-        />
-      ))}
+    <div>
+      <div class="bing-wallpaper">
+        {cn.map((item) => (
+          <Wallpaper
+            url={item.url}
+            enddate={item.enddate}
+            copyright={item.copyright}
+          />
+        ))}
+      </div>
+      <div class="bing-wallpaper">
+        {www.map((item) => (
+          <Wallpaper
+            url={item.url}
+            enddate={item.enddate}
+            copyright={item.copyright}
+          />
+        ))}
+      </div>
     </div>
   );
 }
